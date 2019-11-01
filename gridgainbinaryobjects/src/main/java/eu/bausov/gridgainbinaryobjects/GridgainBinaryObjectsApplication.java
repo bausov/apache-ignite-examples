dc6c41af12768b39716fe07e177d9bd0263db24a
Stanislav Bausov <s.bausov@cardsmobile.com>
2019-11-01 16:39:19 +0300

    binary objects module initial commit

---
+++ gridgainbinaryobjects/src/main/java/eu/bausov/gridgainbinaryobjects/GridgainBinaryObjectsApplication.java
@@ -0,0 +1,17 @@
+package eu.bausov.gridgainbinaryobjects;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Stanislav Bausov on 01.11.2019.
+ */
+@SpringBootApplication
+public class GridgainBinaryObjectsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GridgainBinaryObjectsApplication.class, args);
+    }
+
+}
